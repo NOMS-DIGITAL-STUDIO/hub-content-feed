92e70890d6480be793edbcc11ab7caf20e49d1e5
amjadkhan <amjadkl@btinternet.com>
2017-01-09 23:50:55 +0000

    Updated scripts more cleanup plus two.

--- functest.sh
+++ functest.sh
@@ -3,13 +3,8 @@ echo $CIRCLE_TEST_REPORTS;
 ./gradlew functionaltest;
 STATUS_CODE="$?"
 echo "Exit Code" $STATUS_CODE;
-#if [[ $STATUS_CODE != 0 ]];
-#    then
-#      echo "FAILED TEST COPY REPORTS";
-      mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
-      find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
-      cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS/functionaltest;
-      echo "Returning exit";
-      #exit 1
-#fi
+mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
+find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
+cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS/functionaltest;
+echo "Returning exit";
 exit $STATUS_CODE
