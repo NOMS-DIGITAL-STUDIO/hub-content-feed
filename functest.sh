018fc16db9fae53c1f451ed583ed7106f8fd0d0f
amjadkhan <amjadkl@btinternet.com>
2017-01-09 22:40:24 +0000

    Fix scripts 3.

--- functest.sh
+++ functest.sh
@@ -4,12 +4,9 @@ echo $CIRCLE_TEST_REPORTS;
 if [[ ${PIPESTATUS[0]} != 0 ]];
     then
       echo "FAILED TEST COPY REPORTS";
-      pwd;
-      ls -ltra;
       mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
-      #find . -type f -regex ".*/build/test-results/functionaltest" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
       find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
-      find . -type f -regex ".*/build/reports/functionaltest/*.*" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
+      cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS/functionaltest;
       echo "Returning exit 1";
       exit 1
 fi
