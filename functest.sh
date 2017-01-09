db72e99aa685c14971e448406cbc4bd8a11d16df
amjadkhan <amjadkl@btinternet.com>
2017-01-09 16:50:53 +0000

    Fix scripts.

--- functest.sh
+++ functest.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 ./gradlew functionaltest;
+echo $CIRCLE_TEST_REPORTS
 if [[ ${PIPESTATUS[0]} != 0 ]];
     then
     echo "FAILED TEST COPY REPORTS"
-      mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/
+      pwd;
+      ls -ltra;
+      mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
       find . -type f -regex ".*/build/test-results/functionaltest" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
     exit 1
 fi
