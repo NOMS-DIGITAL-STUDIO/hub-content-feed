79cce1bff440adfdb7750d99373b4169c8f19b44
amjadkhan <amjadkl@btinternet.com>
2017-01-09 16:38:17 +0000

    Fix Script

--- functest.sh
+++ functest.sh
@@ -3,7 +3,7 @@
 if [[ ${PIPESTATUS[0]} != 0 ]];
     then
     echo "FAILED TEST COPY REPORTS"
-      - mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/
-      - find . -type f -regex ".*/build/test-results/functionaltest" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
+      mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/
+      find . -type f -regex ".*/build/test-results/functionaltest" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
     exit 1
 fi
