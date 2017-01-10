d30bffbb54d1d1ac45aed343130e46150808f676
amjadkhan <amjadkl@btinternet.com>
2017-01-10 18:06:16 +0000

    More cleanup of shell script.

--- run-functional-test.sh
+++ run-functional-test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-./gradlew functionaltest;
+./gradlew functionaltest
 STATUS_CODE="$?"
 set -e
-echo "Exit Code" $STATUS_CODE;
+echo "Exit Code" $STATUS_CODE
 mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
-find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
-cp -r build/reports/tests/functionaltest $CIRCLE_TEST_REPORTS/functionaltest/report;
-echo "Returning exit";
+find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \
+cp -r build/reports/tests/functionaltest $CIRCLE_TEST_REPORTS/functionaltest/report
+echo "Returning exit"
 exit $STATUS_CODE
