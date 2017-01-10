e1325cbcdbecd15de59e5e792bc107149f1a8c9b
amjadkhan <amjadkl@btinternet.com>
2017-01-10 12:01:39 +0000

    More cleanup on config.

--- run-functional-test.sh
+++ run-functional-test.sh
@@ -4,6 +4,6 @@ STATUS_CODE="$?"
 echo "Exit Code" $STATUS_CODE;
 mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
 find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
-cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS/functionaltest;
+cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS;
 echo "Returning exit";
 exit $STATUS_CODE
