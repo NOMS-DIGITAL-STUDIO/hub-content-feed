2590f103dcd928fcc76d965623f6186bdfa0e43c
amjadkhan <amjadkl@btinternet.com>
2017-01-10 15:13:27 +0000

    fix bug in script.

--- run-functional-test.sh
+++ run-functional-test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 ./gradlew functionaltest;
 STATUS_CODE="$?"
+set -e
 echo "Exit Code" $STATUS_CODE;
 mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
 find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
-cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS/report;
+cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS/functionaltest/report;
 echo "Returning exit";
 exit $STATUS_CODE
