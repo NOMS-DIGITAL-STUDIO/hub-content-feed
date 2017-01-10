bf5f6bcedc5ff2fd482e81dc37f7fdb3d4c1edc7
amjadkhan <amjadkl@btinternet.com>
2017-01-10 18:11:53 +0000

    More cleanup of shell script 2.

--- run-functional-test.sh
+++ run-functional-test.sh
@@ -4,7 +4,7 @@ STATUS_CODE="$?"
 set -e
 echo "Exit Code" $STATUS_CODE
 mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
-find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \
+find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
 cp -r build/reports/tests/functionaltest $CIRCLE_TEST_REPORTS/functionaltest/report
 echo "Returning exit"
 exit $STATUS_CODE
