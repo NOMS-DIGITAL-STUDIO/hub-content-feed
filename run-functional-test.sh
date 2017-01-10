b8130af09815c535c7ffb751a823163c7d281a3a
amjadkhan <amjadkl@btinternet.com>
2017-01-10 15:33:32 +0000

    fix bug in script 2.

--- run-functional-test.sh
+++ run-functional-test.sh
@@ -5,6 +5,6 @@ set -e
 echo "Exit Code" $STATUS_CODE;
 mkdir -p $CIRCLE_TEST_REPORTS/functionaltest/;
 find . -type f -regex ".*/build/test-results/functionaltest/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/functionaltest/ \;
-cp -r build/reports/functionaltest $CIRCLE_TEST_REPORTS/functionaltest/report;
+cp -r build/reports/tests/functionaltest $CIRCLE_TEST_REPORTS/functionaltest/report;
 echo "Returning exit";
 exit $STATUS_CODE
