4c00adf2dfc200112f6093452eeb7a655b8703f7
Nick Redshaw <nickredshaw@gmail.com>
2017-02-08 17:45:30 +0000

    HUB-144: Laravel spike changes to present an adapted list of categories

--- src/main/java/uk/gov/justice/digital/noms/hub/domain/ContentItem.java
+++ src/main/java/uk/gov/justice/digital/noms/hub/domain/ContentItem.java
@@ -7,6 +7,7 @@ public class ContentItem {
     private String title;
     private String uri;
     private String id;
+    private String category;
 
     public ContentItem(String title, String uri) {
         this.title = title;
